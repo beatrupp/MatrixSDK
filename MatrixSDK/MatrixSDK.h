e6434bcb6bc3639077b9659eb10925e1ba584694
Gustavo Perdomo <gperdomor@gmail.com>
2016-12-06 21:17:46 -0300

    Initial commit

---
+++ MatrixSDK/MatrixSDK.h
@@ -0,0 +1,19 @@
+//
+//  MatrixSDK.h
+//  MatrixSDK
+//
+//  Created by Gustavo Perdomo on 12/6/16.
+//  Copyright © 2016 Gustavo Perdomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MatrixSDK.
+FOUNDATION_EXPORT double MatrixSDKVersionNumber;
+
+//! Project version string for MatrixSDK.
+FOUNDATION_EXPORT const unsigned char MatrixSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatrixSDK/PublicHeader.h>
+
+
