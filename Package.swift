377882e9b1580c41bfaabfbe067e9b79e4209653
Gustavo Perdomo <gperdomor@gmail.com>
2017-02-22 14:30:07 -0300

    Initial deps

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "MatrixSDK"
+    name: "MatrixSDK",
+    dependencies: [
+        .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6),
+        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
+        .Package(url: "https://github.com/Moya/Moya", majorVersion: 8),
+        .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2),
+        .Package(url: "https://github.com/gperdomor/MoyaObjectMapper.git", majorVersion: 1)
+    ],
+    exclude: [
+        "Carthage",
+        "Configs",
+        "Demo",
+        "scripts"
+    ]
 )
